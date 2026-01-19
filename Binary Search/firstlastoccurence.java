e62a13c09645942739bbda6745ac1667d6101d93
Ishita3120 <ishitasahay31@gmail.com>
2026-01-19 23:53:52 +0530

    Add files via upload

---
+++ Binary Search/firstlastoccurence.java
@@ -0,0 +1,55 @@
+import java.util.Scanner;
+
+public class firstlastoccurence {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int[] a = new int[13];
+        System.out.println("enter elements in the array");
+        // array input
+        for (int i = 0; i < a.length; i++) {
+            a[i] = in.nextInt();
+        }
+        System.out.println("enter the element you want to search");
+        int x = in.nextInt();
+        int i = 0;
+        int j = a.length - 1;
+        int mid;
+        int idx = 0;
+        while (i <= j) {
+            mid = (i + j) / 2;
+            if (a[mid] > x) {
+                j = mid - 1;
+            } else if (a[mid] < x) {
+                i = mid + 1;
+            }
+            // element already mil chuka hai you are checking ki vo first occurence hai ya
+            // nhi
+            else {
+                idx = mid;
+                j = mid - 1;
+
+            }
+        }
+        System.out.println("the first occurence of the element is at position" + " " + idx);
+        i = 0;
+        j = a.length - 1;
+        mid = 0;
+        idx = 0;
+        while (i <= j) {
+            mid = (i + j) / 2;
+            if (a[mid] > x) {
+                j = mid - 1;
+            } else if (a[mid] < x) {
+                i = mid + 1;
+            }
+            // element already mil chuka hai you are checking ki vo last occurence hai ya
+            // nhi
+            else {
+                idx = mid;
+                i = mid + 1;
+
+            }
+        }
+        System.out.println("the last  occurence of the element is at position" + " " + idx);
+    }
+}
