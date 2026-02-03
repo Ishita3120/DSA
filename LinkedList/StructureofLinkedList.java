ebf921d5ba3647e50554e783b5594428aac36539
Ishita3120 <ishitasahay31@gmail.com>
2026-02-03 11:28:27 +0530

    Add files via upload

---
+++ LinkedList/StructureofLinkedList.java
@@ -0,0 +1,34 @@
+package LinkedList;
+
+public class StructureofLinkedList {
+    static class Node {
+        int val;
+        Node next;
+
+        Node(int val) {
+            this.val = val;
+        }
+    }
+
+    public static void main(String args[]) {
+        Node a = new Node(10);
+        Node b = new Node(20);
+        Node c = new Node(30);
+        Node d = new Node(40);
+        Node e = new Node(50);
+        a.next = b;
+        b.next = c;
+        c.next = d;
+        d.next = e;
+        System.out.println(a.val);
+        System.out.println(b.val);
+        System.out.println(c.val);
+        System.out.println(d.val);
+        System.out.println(a.next);
+        System.out.println(b.next);
+        System.out.println(c.next);
+        System.out.println(d.next);
+        System.out.println(e.next);
+
+    }
+}
